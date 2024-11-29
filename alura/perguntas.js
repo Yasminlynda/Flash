68e697c0f6dcf775d6e0a9453ec087ecbbb84555
Yasmin <passosyasmin09@gmail.com>
2024-11-29 10:35:12 -0300

    Add files via upload

---
+++ alura/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Curiosidade',
+    'Qual é o maior rio da América do Sul??',
+    'O rio Amazonas'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Russia?',
+    'A capital da Russia é Moscovo'
+)
+
+criaCartao(
+    'Astrologia',
+    'Qual o planeta mais distante do sol?',
+    'O planeta mais distante do sol é Netuno'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz tchau em Inglês?',
+    'Oi em ingles é bye (bai)'
+)
